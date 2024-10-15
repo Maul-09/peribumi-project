d6f62a26b59a4916bfaad4a4cdf92124e911a961
Maul_09 <azism.3930@gmail.com>
2024-10-15 22:56:25 +0700

    Maul 23:56

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -1,106 +1,112 @@
 <x-layout>
-    <div id="home">
-        <h1>PERIBUMI CONSULTANT</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam sint voluptate consequatur alias recusandae
-            explicabo temporibus, ad dolorum nisi, voluptatum ab non minima totam iure repellat sapiente nulla
-            repellendus
-            suscipit?</p>
-        <button>TENTANG KAMI</button>
-    </div>
+    <section class="banner" id="home">
+        <div class="textBx">
+            <h2>PERIBUMI CONSULTANT</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam sint voluptate consequatur alias recusandae
+                explicabo temporibus, ad dolorum nisi, voluptatum ab non minima totam iure repellat sapiente nulla
+                repellendus
+                suscipit?</p>
+            <a href="" class="btn">TENTANG KAMI</a>
+        </div>
+    </section>
 
-    <div id="about">
-        <h3>Tentang Peribumi Consultan</h3>
-        <h1>Kami hadir sejak tahun 2023</h1>
+    <section class=" about" id="about">
         <div>
+            <h3>Tentang Peribumi Consultan</h3>
+            <h1>Kami hadir sejak tahun 2023</h1>
             <div>
-                <button>Visi</button>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, quo mollitia tempora enim
+                <div>
+                    <button>Visi</button>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, quo mollitia tempora enim
                     perspiciatis
                     natus blanditiis ullam at maiores, nobis reiciendis magni. Eius officiis dicta iure possimus, illo
                     recusandae ipsam.</p>
-            </div>
-            <div>
-                <button>Misi</button>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum voluptates ab assumenda aliquam
+                </div>
+                <div>
+                    <button>Misi</button>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum voluptates ab assumenda aliquam
                     similique, labore minus quo totam incidunt asperiores explicabo eligendi? Possimus placeat laborum
                     dolorem accusantium atque expedita molestiae.</p>
+                </div>
             </div>
-        </div>
-
-        <div>
-            <img src="#" alt="foto">
-        </div>
-
-        <div>
-            <h1>Tentang Kami</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, assumenda eaque unde nostrum neque
+            <div>
+                <img src="#" alt="foto">
+            </div>
+            <div>
+                <h1>Tentang Kami</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, assumenda eaque unde nostrum neque
                 architecto esse maxime optio exercitationem quam! Quos rem aspernatur ab cupiditate nisi incidunt
                 molestiae aliquid delectus.</p>
-            <button>TIM PERIBUMI</button>
+                <button>TIM PERIBUMI</button>
+            </div>
         </div>
-    </div>
+    </section>
 
-    <div id="product">
-        <div class="title">
-            <h2>PRODUK & LAYANAN</h2>
-        </div>
-        <div class="card">
-            <div class="box">
-                <h3>Management Business</h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo nemo pariatur autem libero eveniet,
+    <section>
+        <div id="product">
+            <div class="title">
+                <h2>PRODUK & LAYANAN</h2>
+            </div>
+            <div class="card">
+                <div class="box">
+                    <h3>Management Business</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo nemo pariatur autem libero eveniet,
                     animi provident itaque reiciendis veniam nesciunt, nam ipsam hic minima in dolorum vel odio ad
                     dolor!</p>
-                <a href="#"><img src="#" alt="foto"></a>
-            </div>
-            <div class="box">
-                <h3>Training Center</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga beatae magnam blanditiis nobis
+                    <a href="#"><img src="#" alt="foto"></a>
+                </div>
+                <div class="box">
+                    <h3>Training Center</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga beatae magnam blanditiis nobis
                     repudiandae tempore, repellendus error sequi ab suscipit sapiente aliquid distinctio aperiam esse
                     autem animi deserunt, sint impedit.</p>
-                <a href="#"><img src="#" alt="foto"></a>
-            </div>
-            <div class="box">
-                <h3>Digital Solution</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus pariatur esse quaerat minima
+                    <a href="#"><img src="#" alt="foto"></a>
+                </div>
+                <div class="box">
+                    <h3>Digital Solution</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus pariatur esse quaerat minima
                     obcaecati repellat quasi sequi, incidunt quo porro beatae! Vel odit sapiente sint adipisci,
                     dignissimos laboriosam assumenda veniam?</p>
-                <a href="#"><img src="#" alt="foto"></a>
-            </div>
-            <div class="box">
-                <h3>Personal Development</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga consectetur consequatur magnam
+                    <a href="#"><img src="#" alt="foto"></a>
+                </div>
+                <div class="box">
+                    <h3>Personal Development</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga consectetur consequatur magnam
                     incidunt harum quasi dolores vel, repellat officia aliquam quaerat accusamus sint, eos cupiditate
                     eum sed, voluptas assumenda commodi?</p>
-                <a href="#"><img src="#" alt="foto"></a>
-            </div>
-            <div class="box">
-                <h3>Event Organizer</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi cupiditate obcaecati officia velit
+                    <a href="#"><img src="#" alt="foto"></a>
+                </div>
+                <div class="box">
+                    <h3>Event Organizer</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi cupiditate obcaecati officia velit
                     nulla. Vitae, tempora? Numquam aliquid necessitatibus quas consequatur inventore mollitia dolores
                     expedita, at iste incidunt cupiditate consequuntur.</p>
-                <a href="#"><img src="#" alt="foto"></a>
+                    <a href="#"><img src="#" alt="foto"></a>
+                </div>
             </div>
         </div>
-    </div>
+    </section>
 
-    <div id="mitra">
-        <div class="title">
-            <h2>CLIENT & KEMITRAAN</h2>
+    <section>
+        <div id="mitra">
+            <div class="title">
+                <h2>CLIENT & KEMITRAAN</h2>
+            </div>
+            <div>
+                <img src="#" alt="logo">
+            </div>
+            <div>
+                <img src="#" alt="logo">
+            </div>
+            <div>
+                <img src="#" alt="logo">
+            </div>
+            <div>
+                <img src="#" alt="logo">
+            </div>
+            <div>
+                <img src="#" alt="logo">
+            </div>
         </div>
-        <div>
-            <img src="#" alt="logo">
-        </div>
-        <div>
-            <img src="#" alt="logo">
-        </div>
-        <div>
-            <img src="#" alt="logo">
-        </div>
-        <div>
-            <img src="#" alt="logo">
-        </div>
-        <div>
-            <img src="#" alt="logo">
-        </div>
-    </div>
+    </section>
 </x-layout>
--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,46 +1,44 @@
-<footer id="footer">
-    <div class="title">
-        <img src="#" alt="logo">
-        <h2>PERIBUMI CONSULTANT</h2>
-    </div>
+    <footer id="footer">
+        <div class="title">
+            <img src="#" alt="logo">
+            <h2>PERIBUMI CONSULTANT</h2>
+        </div>
 
-    <div>
-        <h3>Tentang kami</h3>
-        <p>
+        <div>
+            <h3>Tentang kami</h3>
+            <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum sed dolorum ipsam recusandae ab fugiat
             dolor facere laborum in placeat minus ut vel ducimus perferendis doloribus obcaecati saepe, vitae deserunt?
-        </p>
-    </div>
+            </p>
+        </div>
 
-    <div>
-        <h3>Produk</h3>
-        <a href="#">Management Business</a>
-        <a href="#">Training Center</a>
-        <a href="#">Digital Solution</a>
-        <a href="#">Personal Development</a>
-        <a href="#">Event Organizer</a>
-    </div>
+        <div>
+            <h3>Produk</h3>
+            <a href="#">Management Business</a>
+            <a href="#">Training Center</a>
+            <a href="#">Digital Solution</a>
+            <a href="#">Personal Development</a>
+            <a href="#">Event Organizer</a>
+        </div>
 
-    <div>
-        <h3>Navigasi</h3>
-        <a href="#home">Home</a>
-        <a href="#about">About Us</a>
-        <a href="#product">Products Us</a>
-        <a href="#mitra">Mitra</a>
-        <a href="#footer">Contact Us</a>
-    </div>
+        <div>
+            <h3>Navigasi</h3>
+            <a href="#home">Home</a>
+            <a href="#about">About Us</a>
+            <a href="#product">Products Us</a>
+            <a href="#mitra">Mitra</a>
+            <a href="#footer">Contact Us</a>
+        </div>
 
-    <div>
-        <h3>Sosial Media</h3>
-        <a href="#"><img src="#" alt="icon"></a>
-        <a href="#"><img src="#" alt="icon"></a>
-        <a href="#"><img src="#" alt="icon"></a>
-    </div>
+        <div>
+            <h3>Sosial Media</h3>
+            <a href="#"><img src="#" alt="icon"></a>
+            <a href="#"><img src="#" alt="icon"></a>
+            <a href="#"><img src="#" alt="icon"></a>
+        </div>
 
-    <div>
-        <iframe
-            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d990.1381707338038!2d107.64095861518679!3d-6.943938839532725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sid!2sid!4v1728965370957!5m2!1sid!2sid"
-            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"></iframe>
-    </div>
-</footer>
+        <div>
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d990.1381707338038!2d107.64095861518679!3d-6.943938839532725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sid!2sid!4v1728965370957!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </footer>
