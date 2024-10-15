b9b5aa74a68b1d9d2afcb826d9f32d0a327982e6
wafihaz <waliyafy17@gmail.com>
2024-10-15 13:50:16 +0700

    yapi

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -2,7 +2,7 @@
     <a href="#home" class="logo"><img src="{{ asset('image/logo-peribumi.png') }}" alt="logo">PERIBUMI CONSULTANT</a>
     <div class="toggle"></div>
     <ul class="menu">
-        <li><a href="{{routes('beranda')  }}#home">Home</a></li>
+        <li><a href="{{routes('beranda')  }}#home">Imah</a></li>
         <li><a href="{{routes('beranda')  }}#about">About us</a></li>
         <li><a href="{{routes('beranda')  }}#product">Product</a></li>
         <li><a href="{{routes('beranda')  }}#mitra">Mitra</a></li>
