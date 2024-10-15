48d7e73c9d056afdb580bbcd960be10ec5ed725c
amaricee <mhmdramaa1109@gmail.com>
2024-10-15 14:04:18 +0700

    14.04

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -1,12 +1,13 @@
 <header class="sticky">
-    <a href="#home" class="logo"><img src="{{ asset('image/logo-peribumi.png') }}" alt="logo">PERIBUMI CONSULTANT</a>
+    <a href="#home" class="logo"><img src="{{ asset('image/logo-peribumi.png') }}" alt="logo">PERIBUMI
+        CONSULTANT</a>
     <div class="toggle"></div>
     <ul class="menu">
-        <li><a href="{{routes('beranda')  }}#home">Imah</a></li>
-        <li><a href="{{routes('beranda')  }}#about">About us</a></li>
-        <li><a href="{{routes('beranda')  }}#product">Product</a></li>
-        <li><a href="{{routes('beranda')  }}#mitra">Mitra</a></li>
-        <li><a href="{{routes('beranda')  }}#footer">Contact us</a></li>
-        <li><a href="{{routes('beranda')  }}#">Sign in</a></li>
+        <li><a href="{{ route('beranda') }}#home">home</a></li>
+        <li><a href="{{ route('beranda') }}#about">About us</a></li>
+        <li><a href="{{ route('beranda') }}#product">Product</a></li>
+        <li><a href="{{ route('beranda') }}#mitra">Mitra</a></li>
+        <li><a href="{{ route('beranda') }}#footer">Contact us</a></li>
+        <li><a href="{{ route('beranda') }}#">Sign in</a></li>
     </ul>
 </header>
