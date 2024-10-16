66b6d9b8c36b123b45040d535868ca14723e3292
Maul_09 <azism.3930@gmail.com>
2024-10-16 15:03:39 +0700

    Maul 15:03

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -6,15 +6,15 @@
         <li><a href="{{ route('beranda') }}#home">Home</a></li>
         <li><a href="{{ route('beranda') }}#about">About us</a></li>
         <li class="dropdown">
-            <button class="dropbtn">Product</button>
+            <a href="javascript:void(0)" class="dropbtn">Product</a>
             <div class="dropdown-content">
-                <a href="manajemen">Management Business</a>
-                <a href="#">Training Center</a>
-                <a href="#">Digital Solution</a>
-                <a href="#">Personal Development</a>
-                <a href="#">Event Organizer</a>
+              <a href="#">Management Bussines</a>
+              <a href="#">Training Center</a>
+              <a href="#">Digital Solution</a>
+              <a href="#">Personal Development</a>
+              <a href="#">Event Organizer</a>
             </div>
-        </li>
+          </li>
         <li><a href="{{ route('beranda') }}#mitra">Mitra</a></li>
         <li><a href="{{ route('beranda') }}#footer">Contact us</a></li>
         <li><a href="{{ route('beranda') }}#">Sign in</a></li>
