f9d8bf464ff929bb736dfcafb7b7630b8f57bc7f
Maul_09 <azism.3930@gmail.com>
2024-10-17 15:59:35 +0700

    Button Done Ngabbb

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,16 @@
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function() {
     var header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+const button = document.getElementById('dropdownButton');
+const menu = document.getElementById('dropdownMenu');
+button.addEventListener('click', () => {
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+});
+
+window.addEventListener('click', (event) => {
+    if (!button.contains(event.target) && !menu.contains(event.target)) {
+        menu.style.display = 'none';
+    }
 });
