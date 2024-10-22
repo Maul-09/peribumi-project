4b6fa0f3db9239b47bc06a5e12b6731cf1ed8092
wafihaz <waliyafy17@gmail.com>
2024-10-22 10:33:30 +0700

    dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,66 @@
+<x-layout>
+    <div>
+        <h2>ADMINISTRATOR</h2>
+        <hr>
+        <div>
+            <img src="" alt="">
+            <p>Peribumi Consultan Admin</p>
+            <i href=""></i>
+            <i href=""></i>
+            <i href=""></i>
+        </div>
+        <hr>
+        <div>
+            <i href=""></i>
+            <p>Dashboard</p>
+        </div>
+        <div>
+            <i href=""></i>
+            <p>Manajemen Produk</p>
+        </div>
+        <div>
+            <i href=""></i>
+            <p>LMS</p>
+        </div>
+    </div>
+    <div>
+        <h1>Selamat Datang, Admin Peribumi</h1>
+        <hr>
+        <div>
+            <div>
+                <div>
+                    <i href=""></i>
+                    <p>Member</p>
+                </div>
+                <p>500</p>
+            </div>
+            <div>
+                <div>
+                    <i href=""></i>
+                    <p>Produk</p>
+                </div>
+                <p>500</p>
+            </div>
+            <div>
+                <div>
+                    <i href=""></i>
+                    <p>Ulasan</p>
+                </div>
+                <p>5.00</p>
+            </div>
+        </div>
+        <div>
+            <div>
+                <p>Task Manager</p>
+                <hr>
+            </div>
+            <div>
+                <p>Rating</p>
+                <hr>
+            </div>
+        </div>
+        <div>
+            <p>Statistik Pengunjung</p>
+        </div>
+    </div>
+</x-layout>
