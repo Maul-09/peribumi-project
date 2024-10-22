9fabcf9cc0f5e7b63789ebc885e0951a25ca965b
Maul_09 <azism.3930@gmail.com>
2024-10-22 13:47:06 +0700

    Directori Rombak

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,12 @@ Route::get('/admin', function () {
     return view('admin\dashboard');
 });
 
-Route::get('/', 'App\Http\Controllers\PeribumiController@beranda')->name('beranda');
-Route::get('/manajemen', 'App\Http\Controllers\PeribumiController@manajemen')->name('manajemen');
-Route::get('/training', 'App\Http\Controllers\PeribumiController@training')->name('training');
-Route::get('/digital', 'App\Http\Controllers\PeribumiController@digital')->name('digital');
-Route::get('/personal', 'App\Http\Controllers\PeribumiController@personal')->name('personal');
-Route::get('/event', 'App\Http\Controllers\PeribumiController@event')->name('event');
+Route::get('/user', 'App\Http\Controllers\PeribumiController@beranda')->name('user/beranda');
+Route::get('/user/manajemen', 'App\Http\Controllers\PeribumiController@manajemen')->name('user/manajemen');
+Route::get('/user/training', 'App\Http\Controllers\PeribumiController@training')->name('user/training');
+Route::get('/user/digital', 'App\Http\Controllers\PeribumiController@digital')->name('user/digital');
+Route::get('/user/personal', 'App\Http\Controllers\PeribumiController@personal')->name('user/personal');
+Route::get('/user/event', 'App\Http\Controllers\PeribumiController@event')->name('user/event');
 
 Route::get('/auth/logreg', 'App\Http\Controllers\PeribumiController@authentication')->name('logreg');
 Route::post('/signup', 'App\Http\Controllers\PeribumiController@signup')->name('signup');
