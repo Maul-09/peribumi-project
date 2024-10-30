57f984d732b46b30dcd4f0211b85ed862291d043
Maul_09 <azism.3930@gmail.com>
2024-10-30 13:06:42 +0700

    seting-seting

--- resources/views/auth/logreg.blade.php
+++ resources/views/auth/logreg.blade.php
@@ -2,12 +2,22 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/head-footer-style/logreg.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Register</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="{{ asset('css/head-footer-style/logreg.css') }}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!-- Vendor CSS Files -->
+<link href="{{ asset('aset/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+<link href="{{ asset('aset/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+<link href="{{ asset('aset/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+<link href="{{ asset('aset/assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+<link href="{{ asset('aset/assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+<link href="{{ asset('aset/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+<link href="{{ asset('aset/assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+<!-- Template Main CSS File -->
+<link href="{{ asset('aset/assets/css/style.css') }}" rel="stylesheet">
+<title>Peribumi Consultant - Register</title>
 </head>
 
 <body class="log">
@@ -30,21 +40,11 @@
                     {{ session('Failed') }}
                 </div>
               @endif
-              <input type="submit" value="Login" class="btn solid" />
-
-              <p class="social-text">Or Sign in with social platforms</p>
+              <input type="submit" class="btn" value="Sign up" />
+              <p class="social-text">Or signin with Google</p>
               <div class="social-media">
                 <a href="#" class="social-icon">
-                  <i class="fab fa-facebook-f"></i>
-                </a>
-                <a href="#" class="social-icon">
-                  <i class="fab fa-twitter"></i>
-                </a>
-                <a href="#" class="social-icon">
                   <i class="fab fa-google"></i>
-                </a>
-                <a href="#" class="social-icon">
-                  <i class="fab fa-linkedin-in"></i>
                 </a>
               </div>
             </form>
@@ -57,7 +57,7 @@
                 @error('name')
 
                 <p>{{ $message }}</p>
-                  
+
                 @enderror
               </div>
               <div class="input-field">
@@ -66,7 +66,7 @@
                 @error('email')
 
                 <p>{{ $message }}</p>
-                  
+
                 @enderror
               </div>
               <div class="input-field">
@@ -75,7 +75,7 @@
                 @error('username')
 
                 <p>{{ $message }}</p>
-                  
+
                 @enderror
               </div>
               <div class="input-field">
@@ -84,24 +84,14 @@
                 @error('password')
 
                 <p>{{ $message }}</p>
-                  
+
                 @enderror
               </div>
               <input type="submit" class="btn" value="Sign up" />
-
-              <p class="social-text">Or Sign up with social platforms</p>
+              <p class="social-text">Or signup with Google</p>
               <div class="social-media">
                 <a href="#" class="social-icon">
-                  <i class="fab fa-facebook-f"></i>
-                </a>
-                <a href="#" class="social-icon">
-                  <i class="fab fa-twitter"></i>
-                </a>
-                <a href="#" class="social-icon">
                   <i class="fab fa-google"></i>
-                </a>
-                <a href="#" class="social-icon">
-                  <i class="fab fa-linkedin-in"></i>
                 </a>
               </div>
             </form>
@@ -141,6 +131,17 @@
       </div>
 
 <script src="{{ asset('js/logreg.js') }}"></script>
+<script src="{{ asset('aset/assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ asset('aset/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('aset/assets/vendor/chart.js/chart.umd.js') }}"></script>
+<script src="{{ asset('aset/assets/vendor/echarts/echarts.min.js') }}"></script>
+<script src="{{ asset('aset/assets/vendor/quill/quill.js') }}"></script>
+<script src="{{ asset('aset/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+<script src="{{ asset('aset/assets/vendor/tinymce/tinymce.min.js') }}"></script>
+<script src="{{ asset('aset/assets/vendor/php-email-form/validate.js') }}"></script>
+
+<!-- Template Main JS File -->
+<script src="{{ asset('aset/assets/js/main.js') }}"></script>
 </body>
 
 </html>
--- resources/views/user/components/layout.blade.php
+++ resources/views/user/components/layout.blade.php
@@ -2,15 +2,26 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ $title }}">
-    <link rel="stylesheet" href="{{ asset('css/head-footer-style/footer.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/skin/color-palete.css') }}">
-    <link rel="icon" type="image/png" href="{{ asset('image/favicon.ico') }} ">
-    <link rel="stylesheet" href="{{ asset('css/head-footer-style/style-nav.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="{{ $title }}">
+<link rel="stylesheet" href="{{ asset('css/head-footer-style/footer.css') }}">
+<link rel="stylesheet" href="{{ asset('css/skin/color-palete.css') }}">
+<link rel="icon" type="image/png" href="{{ asset('image/favicon.ico') }} ">
+<link rel="stylesheet" href="{{ asset('css/head-footer-style/style-nav.css') }}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!-- Vendor CSS Files -->
+<link href="{{ asset('aset/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+<link href="{{ asset('aset/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+<link href="{{ asset('aset/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+<link href="{{ asset('aset/assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+<link href="{{ asset('aset/assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+<link href="{{ asset('aset/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+<link href="{{ asset('aset/assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+<!-- Template Main CSS File -->
+<link href="{{ asset('aset/assets/css/style.css') }}" rel="stylesheet">
+<title>Peribumi Consultant - Wellcome</title>
     <title>{{ $name }}</title>
 </head>
 
@@ -21,8 +32,19 @@
     </main>
     <x-footer></x-footer>
 
-    <script src="{{ asset('js/script.js') }}"></script>
-    <script src="{{ asset('js/nav.js') }}"></script>
+<script src="{{ asset('js/script.js') }}"></script>
+<script src="{{ asset('js/nav.js') }}"></script>
+<script src="{{ asset('aset/assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ asset('aset/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('aset/assets/vendor/chart.js/chart.umd.js') }}"></script>
+<script src="{{ asset('aset/assets/vendor/echarts/echarts.min.js') }}"></script>
+<script src="{{ asset('aset/assets/vendor/quill/quill.js') }}"></script>
+<script src="{{ asset('aset/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+<script src="{{ asset('aset/assets/vendor/tinymce/tinymce.min.js') }}"></script>
+<script src="{{ asset('aset/assets/vendor/php-email-form/validate.js') }}"></script>
+
+<!-- Template Main JS File -->
+<script src="{{ asset('aset/assets/js/main.js') }}"></script>
 </body>
 
 </html>
