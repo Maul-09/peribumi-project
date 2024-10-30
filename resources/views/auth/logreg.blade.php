38d157213b0772aedc21ad7115bb75213c75a7e3
Maul_09 <azism.3930@gmail.com>
2024-10-30 13:31:35 +0700

    h

--- resources/views/auth/logreg.blade.php
+++ resources/views/auth/logreg.blade.php
@@ -7,16 +7,6 @@
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link rel="stylesheet" href="{{ asset('css/head-footer-style/logreg.css') }}">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<!-- Vendor CSS Files -->
-<link href="{{ asset('aset/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-<link href="{{ asset('aset/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-<link href="{{ asset('aset/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-<link href="{{ asset('aset/assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
-<link href="{{ asset('aset/assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
-<link href="{{ asset('aset/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-<link href="{{ asset('aset/assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
-<!-- Template Main CSS File -->
-<link href="{{ asset('aset/assets/css/style.css') }}" rel="stylesheet">
 <title>Peribumi Consultant - Register</title>
 </head>
 
@@ -131,17 +121,6 @@
       </div>
 
 <script src="{{ asset('js/logreg.js') }}"></script>
-<script src="{{ asset('aset/assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
-<script src="{{ asset('aset/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('aset/assets/vendor/chart.js/chart.umd.js') }}"></script>
-<script src="{{ asset('aset/assets/vendor/echarts/echarts.min.js') }}"></script>
-<script src="{{ asset('aset/assets/vendor/quill/quill.js') }}"></script>
-<script src="{{ asset('aset/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
-<script src="{{ asset('aset/assets/vendor/tinymce/tinymce.min.js') }}"></script>
-<script src="{{ asset('aset/assets/vendor/php-email-form/validate.js') }}"></script>
-
-<!-- Template Main JS File -->
-<script src="{{ asset('aset/assets/js/main.js') }}"></script>
 </body>
 
 </html>
