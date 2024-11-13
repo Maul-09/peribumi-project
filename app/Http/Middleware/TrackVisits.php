421c357417cb38df93c29882fae3b24979d0794b
wafihaz <waliyafy17@gmail.com>
2024-11-13 10:02:33 +0700

    chart

--- app/Http/Middleware/TrackVisits.php
+++ app/Http/Middleware/TrackVisits.php
@@ -5,15 +5,17 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use App\Models\Visit;
+use Carbon\Carbon;
 
 class TrackVisits
 {
     public function handle(Request $request, Closure $next)
     {
         $url = $request->path();
+        $today = Carbon::now()->format('Y-m-d'); // Ambil tanggal hari ini
 
-        // Cek apakah URL sudah ada di database, jika ada tambahkan count
-        $visit = Visit::where('url', $url)->first();
+        // Cek apakah ada entri URL untuk tanggal hari ini
+        $visit = Visit::where('url', $url)->whereDate('created_at', $today)->first();
 
         if ($visit) {
             $visit->increment('count');
@@ -24,4 +26,3 @@ class TrackVisits
         return $next($request);
     }
 }
-
