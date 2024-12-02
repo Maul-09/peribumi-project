cee97be0ae16efb5f3781dd60497123028d300bd
wafihaz <waliyafy17@gmail.com>
2024-12-02 16:35:25 +0700

    push

--- app/Http/Controllers/PeribumiController.php
+++ app/Http/Controllers/PeribumiController.php
@@ -16,33 +16,50 @@ class PeribumiController extends Controller
     {
         $kat = ['Pengembangan Bisnis', 'Pendampingan Manajemen Bisnis'];
 
-        session(['kategori' => $kat, 'route' => 'manajemen.admin']);
+        session(['kategori' => $kat]);
 
         return view('user.manajemen');
     }
 
     public function training()
     {
+        $kat = ['Pelatihan Calon Tenaga Kerja', 'Pelatihan Tenaga Kerja Aktif'];
+
+        session(['kategori' => $kat, 'route' => 'training.admin']);
+
         return view('user.training');
     }
 
     public function personal()
     {
+        $kat = ['Individu', 'Kelompok'];
+
+        session(['kategori' => $kat, 'route' => 'personal.admin']);
+
         return view('user.personal');
     }
 
     public function digital()
     {
+        $kat = ['Software/Aplikasi/Website', 'Artificial Intelegence'];
+
+        session(['kategori' => $kat, 'route' => 'digital.admin']);
+
         return view('user.digital');
     }
 
     public function event()
     {
+        $kat = ['MICE & Travel Vacation', 'Wedding & Integrated Event'];
+
+        session(['kategori' => $kat, 'route' => 'organizer.admin']);
+        
         return view('user.event');
     }
 
     public function login()
     {
+
         return view('auth.login');
     }
 }
