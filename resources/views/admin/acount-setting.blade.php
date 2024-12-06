32b6f8a13fe3bf07bc5e5b72acd8b06925182d00
Maul_09 <azism.3930@gmail.com>
2024-12-06 14:07:03 +0700

    acount

---
+++ resources/views/admin/acount-setting.blade.php
@@ -0,0 +1,17 @@
+<x-adminlayout>
+    <main id="main" class="main">
+        <div class="pagetitle">
+            <div class="d-flex align-items-center justify-content-between pe-3">
+                <h1>Account Setting</h1>
+                <a href="{{ route('produk.create') }}" class="btn btn-primary btn-sm"><i class="bi bi-plus me-1"></i> Tambah
+                    Data</a>
+            </div>
+            <nav>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{ route('admin') }}">Home</a></li>
+                    <li class="breadcrumb-item">Account Setting</li>
+                    <li class="breadcrumb-item active">Peri bumi Consultant</li>
+                </ol>
+            </nav>
+        </div>
+</x-adminlayout>
