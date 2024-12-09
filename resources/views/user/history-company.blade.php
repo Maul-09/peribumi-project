9458b1db949d3e04b2213c6d62e39bf461cff559
Maul_09 <azism.3930@gmail.com>
2024-12-09 11:19:42 +0700

    histori

---
+++ resources/views/user/history-company.blade.php
@@ -0,0 +1,23 @@
+<x-layout>
+    <x-slot:name>Sejarah Perusahaan</x-slot>
+    <x-slot:title>{{ asset('css/user-style/style-histori.css') }}</x-slot>
+    <section class="banner" id="home">
+        <div class="textBx">
+            <h2>Sejarah Perusahaan</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam sint voluptate consequatur alias
+                recusandae
+                explicabo temporibus, ad dolorum nisi, voluptatum ab non minima totam iure repellat sapiente nulla
+                repellendus
+                suscipit?</p>
+        </div>
+    </section>
+
+    <section class="product-manajemen">
+        <div>
+            <div class="title-produk">
+                <h3>Produk Kami</h3>
+            </div>
+        </div>
+        <br>
+    </section>
+</x-layout>
