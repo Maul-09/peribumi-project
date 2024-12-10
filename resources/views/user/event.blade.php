72b9dd7cb2b6627f058a2eda88a47ad7e19ce265
Maul_09 <azism.3930@gmail.com>
2024-12-10 11:27:18 +0700

    p

--- resources/views/user/event.blade.php
+++ resources/views/user/event.blade.php
@@ -3,7 +3,7 @@
     <x-slot:title>{{ asset('css/user-style/style-event.css') }}</x-slot>
     <section class="banner" id="home">
         <div class="textBx">
-            <h2>ORGANIZER</h2>
+            <h2>EVENT ORGANIZER</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam sint voluptate consequatur alias
                 recusandae
                 explicabo temporibus, ad dolorum nisi, voluptatum ab non minima totam iure repellat sapiente nulla
