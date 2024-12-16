bc2cf6bd61ce50c12b447a32b37e2fb6235c7ac2
Maul_09 <azism.3930@gmail.com>
2024-12-16 10:58:00 +0700

    mm

--- resources/views/user/components/layout.blade.php
+++ resources/views/user/components/layout.blade.php
@@ -12,9 +12,7 @@
     <link rel="stylesheet" href="{{ asset('css/head-footer-style/style-nav.css') }}">
     <link rel="stylesheet" href="{{ asset('css/admin-style/style-card-product.css') }}">
     <link rel="stylesheet" href="{{ asset('css/user-style/style-modalkonfirmasi.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
-        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Peribumi Consultant - Wellcome</title>
     <title>{{ $name }}</title>
 </head>
