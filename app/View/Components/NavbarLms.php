ff6047051a31df564213e36991f9681443414712
wafihaz <waliyafy17@gmail.com>
2024-12-17 11:00:25 +0700

    pembenaran sejati

--- app/View/Components/NavbarLms.php
+++ app/View/Components/NavbarLms.php
@@ -21,6 +21,6 @@ class NavbarLms extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('lms.components.navbar-lms');
+        return view('components.navbar-lms');
     }
 }
