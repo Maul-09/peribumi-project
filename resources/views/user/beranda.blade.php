fe92155ece1f981aa17f578a5bbfca937cdb13b6
Maul_09 <azism.3930@gmail.com>
2024-12-17 11:29:41 +0700

    p

--- resources/views/user/beranda.blade.php
+++ resources/views/user/beranda.blade.php
@@ -24,7 +24,7 @@
     {{-- End Alertt berhasil --}}
     <section class="banner" id="home">
         <div class="textBx">
-            <h2>KonsultanQta "Peri bumi"</h2>
+            <h2>KonsultanQta "Peri umi"</h2>
             <p>Hadir dan berkomitmen untuk membantu memfasilitasi langkah percepatan dalam rangka pengembangan
                 dan atau peningkatan kompetensi serta kapasitas Sumber daya manusia yang dimiliki</p>
         </div>
